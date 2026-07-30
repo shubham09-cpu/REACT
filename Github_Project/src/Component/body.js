96c7497aadc922ba2f3f9b8d776383265855a86c
Shubham Gupta <09shubham09gupta@gmail.com>
2026-07-30 20:16:00 +0530

    Github page update

--- Github_Project/src/Component/body.js
+++ Github_Project/src/Component/body.js
@@ -2,19 +2,29 @@ import { useEffect, useState } from "react";
 
 function Body(){
     const[Profile,setProfile]=useState([]);
+    const[numberofprofile,setnumberofprofile]=useState("");
 
-    async function generateProfile(){
-        const response=await fetch("https://api.github.com/users?per_page=10");
+    async function generateProfile(count){
+        const ran=Math.floor(1+Math.random()*10000);
+        try{
+        const response=await fetch(`https://api.github.com/users?since=${ran}&per_page=${count}`);
         const data=await response.json();
-
         setProfile(data);
+    }
+    catch(error)
+    {
+        console.log("Data is not Defined");
+    }
     }
 
     useEffect(()=>{
-        generateProfile();
+        generateProfile(10);
     },[])
 
     return(
+        <div className="but">
+            <input type="text" className="input" placeholder="Search here" value={numberofprofile} onChange={(e)=>setnumberofprofile(e.target.value)}></input>
+            <button onClick={()=>{generateProfile(Number(numberofprofile))}}>Search Profile</button>
         <div className="profile">
             {
               Profile.map((val)=>{return(
@@ -25,6 +35,7 @@ function Body(){
                     </div>)
                 })
             }
+        </div>
         </div>
     )
 }
